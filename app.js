8cc3fe5b0b515b9d3211b5d56c484197ce2845d4
NNP28 <acc92019@gmail.com>
2022-04-27 12:07:01 +0700

    phu

--- app.js
+++ app.js
@@ -7,3 +7,31 @@ for (let i = 0; i < 20; i++) {
         arr[i][j] = "";
     }
 }
+function play(i, j) {
+    if (checkPlayer1 && isNotDaTa(i, j)) {
+        arr[i][j] = "x";
+        checkPlayer1 = false;
+        if (checkWin()) {
+            if (choose == 1) {
+                alert("Người đi trước chiến thắng!")
+              
+            } else {
+                alert("người đi sau chiến thắng");
+               
+            }
+        }
+
+    } else if (!checkPlayer1 && isNotDaTa(i, j)) {
+        arr[i][j] = 'o';
+        checkPlayer1 = true;
+        if (checkWin()) {
+            if (choose == 2) {
+                alert("Người đi trước chiến thắng!")
+
+            } else {
+                alert("người đi sau chiến thắng")
+            }
+        }
+    }
+    display();
+}
